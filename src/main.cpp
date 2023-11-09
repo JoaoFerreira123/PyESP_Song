d0c4bb97d3012ae0902ae5f33072403854f2d9c3
João Ferreira <100808096+JoaoFerreira123@users.noreply.github.com>
2023-11-08 22:20:31 -0300

    in progress....

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,41 @@
 
 void setup(){
   Serial.begin(115200);
-  pinMode(4, INPUT);
+  pinMode(32, INPUT);
+  pinMode(33, INPUT);
+  pinMode(27, INPUT);
+  pinMode(14, INPUT);
+  pinMode(12, INPUT);
 }
 
 
 void loop(){
-  Serial.println(touchRead(4));
+  Serial.println(touchRead(32));
+  Serial.println(touchRead(33));
+  Serial.println(touchRead(27));
+  Serial.println(touchRead(14));
+  Serial.println(touchRead(12));
+
   delay(100);
-  /*if(touchRead(4) < 60){
-    Serial.println('1');
-  }else{
-    Serial.println('0');
-  }
-  delay(100);
-  */
 }
+
+//Código feito anteriormente, pensando em identificar cada planta. Porém não há necessidade.
+/*  int plantaA = touchRead(32);
+  String sendA = "A"+String(plantaA);
+  Serial.println(sendA); //talvez de ruim pq ele vai mandar constantemente... Talvez tenha que colocar um attachInterrupt ou um if aqui pra só mandar se tiver variacao
+
+  int plantaB = touchRead(33);
+  String sendB = "B"+String(plantaB);
+  Serial.println(sendB);
+
+  int plantaC = touchRead(27);
+  String sendC = "C"+String(plantaC);
+  Serial.println(sendC);
+
+  int plantaD = touchRead(14);
+  String sendD = "D"+String(plantaD);
+  Serial.println(sendD);
+
+  int plantaE = touchRead(12);
+  String sendE = "E"+String(plantaE);
+  Serial.println(sendE);*/
--- src/teste.py
+++ src/teste.py
@@ -1,11 +1,42 @@
-test_string = "b'83\r\n'"
+from random import *
+import serial
+import time
 
-# printing original string 
-print("The original string : " + test_string)
+from pygame import mixer 
 
-# using List comprehension + isdigit() +split()
-# getting numbers from string 
-res = [int(i) for i in test_string.split() if i.isdigit()]
 
-# print result
-print("The numbers list is :" + str(res))
+
+# Starting the mixer 
+mixer.init() 
+
+# Loading the song 
+sons = [r"D:\Projetos\PyESP_Song\PyESP_Song\src\naometoque.mp3", r"D:\Projetos\PyESP_Song\PyESP_Song\src\saifora.mp3"]
+
+mixer.music.load(sons[randint(0,1)]) 
+#nao esta funcionando a parte do random pois está fora do while. Ele escolhe e seleciona só uma vez o som
+#tem que rodar isso dentro do loop para escolher continuamente. tentei rapidamente mas n foi
+
+# Setting the volume 
+mixer.music.set_volume(1) 
+
+# Start playing the song 
+print("Hey")
+
+esp32 = serial.Serial('COM3', 115200)
+touchInfo = ""
+
+while True:
+    originalString = str(esp32.readline())
+    print(len(originalString))
+    if len(originalString) == 10:
+        touchInfo = originalString[2:5]
+    elif len(originalString) == 9:
+        touchInfo = originalString[2:4]
+    elif len(originalString) == 8:
+        touchInfo = originalString[2]
+
+    print(touchInfo)
+    #time.sleep(0.1)
+    if(int(touchInfo) < 20):
+        mixer.music.play() 
+        
