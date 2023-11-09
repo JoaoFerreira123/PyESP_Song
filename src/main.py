8aef6f6e577639390268d4aeeeb1dcecae7d507c
João Ferreira <100808096+JoaoFerreira123@users.noreply.github.com>
2023-11-09 13:21:29 -0300

    final Version

--- src/main.py
+++ src/main.py
@@ -1,28 +1,26 @@
+from random import *
 import serial
-import time
-
-from pygame import mixer 
-
+from time import sleep
+from pygame import * 
 
 
 # Starting the mixer 
 mixer.init() 
 
-# Loading the song 
-mixer.music.load(r"D:\Projetos\PyESP_Song\PyESP_Song\src\naometoque.mp3") 
-
 # Setting the volume 
 mixer.music.set_volume(1) 
 
 # Start playing the song 
+print("Hey")
 
-
-esp32 = serial.Serial('COM5', 115200)
+esp32 = serial.Serial('COM29', 115200)
 touchInfo = ""
 
+
 while True:
+    choice = randint(0,5)
     originalString = str(esp32.readline())
-    #print(len(originalString))
+    print(len(originalString))
     if len(originalString) == 10:
         touchInfo = originalString[2:5]
     elif len(originalString) == 9:
@@ -31,7 +29,26 @@ while True:
         touchInfo = originalString[2]
 
     print(touchInfo)
-    time.sleep(0.1)
+
     if(int(touchInfo) < 20):
-        mixer.music.play() 
-        
+        match choice:
+            case 0:
+                mixer.music.load(r"C:\Users\gabri\OneDrive\Área de Trabalho\Plantas Falantes\naometoque.mp3")
+                mixer.music.play()
+                sleep(0.1)
+            case 1:
+                mixer.music.load(r"C:\Users\gabri\OneDrive\Área de Trabalho\Plantas Falantes\Estou com sede, me regue.mp3")
+                mixer.music.play()
+                sleep(0.1)
+            case 2:
+                mixer.music.load(r"C:\Users\gabri\OneDrive\Área de Trabalho\Plantas Falantes\Cuide da natureza.mp3")
+                mixer.music.play()
+                sleep(0.1)
+            case 3:
+                mixer.music.load(r"C:\Users\gabri\OneDrive\Área de Trabalho\Plantas Falantes\Cuidado com as minhas folhas.mp3")
+                mixer.music.play()
+                sleep(0.1)
+            case 4:
+                mixer.music.load(r"C:\Users\gabri\OneDrive\Área de Trabalho\Plantas Falantes\aaaaLuciana (mp3cut.net).mp3")
+                mixer.music.play()
+                sleep(0.1)
